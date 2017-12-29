60bf6a323c6df86c6ac75078b482749258e5deeb
harshitm98 <harshitmah98@gmail.com>
2017-12-29 14:46:08 +0530

    Login Page

---
+++ app/src/main/java/com/example/android/bajajhome/Login/SplashActivity.java
@@ -0,0 +1,52 @@
+package com.example.android.bajajhome.Login;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.android.bajajhome.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 1500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+//                if(FirebaseAuth.getInstance().getCurrentUser()!= null){
+//                    Intent i = new Intent(SplashActivity.this, MainActivity.class);
+//                    startActivity(i);
+//                    finish();
+//                }
+//                else{
+//                    Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+//                    startActivity(i);
+//                    finish();
+//                }
+                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
